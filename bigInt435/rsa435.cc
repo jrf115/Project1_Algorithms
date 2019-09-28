7a77b0a1a8aa77116af19120af4889980fd83884
jrf115 <jrf115@zips.uakron.edu>
2019-09-27 20:00:08 -0400

    Part1: Implementing Fermat Test and maybe use ext Euclidean Alg?

--- bigInt435/rsa435.cc
+++ bigInt435/rsa435.cc
@@ -3,11 +3,16 @@
 // Standard libraries
 #include <string>
 #include <iostream>
-#include <stdlib.h> 
+#include <stdlib.h>
+#include <fstream>
 
 
 // 'BigIntegerLibrary.hh' includes all of the library headers.
 #include "BigIntegerLibrary.hh"
+
+// My function implementations for doing the Fermat Test
+void makePrime(BigUnsigned &p);
+bool fermatTest(BigUnsigned p);
 
 int main(){
 	/* The library throws 'const char *' error messages when things go
@@ -15,7 +20,39 @@ int main(){
 	 * one.  Your C++ compiler might need a command-line option to compile
 	 * code that uses exceptions. */
 	try {
-		      
+	  std::cout << "Implementing Fermat Test\n";
+	  BigUnsigned p,q,e(3),d;
+	  makePrime(p);
+	  std::cout << "P is equal to: " << p << std::endl;
+	  makePrime(q);
+	  std::cout << "Q is equal to: " << q << std::endl;
+	  std::ofstream file;
+	  file.open("p_q.txt");
+	  file << p << '\n' << q;
+	  file.close();
+
+	  BigUnsigned n = p * q ;
+	  BigUnsigned theta_N = (p - 1) * (q - 1);
+	  // Find odd number e that is relatively prime to n
+	  while (gcd(e, theta_N) != 1)
+	  	e += 2;
+	  std::cout << "Found e: " << e << std::endl;
+
+	  //  Compute d, which is the multiplicative inverse of e
+	  d = modinv(e, theta_N);
+	  std::cout << "Found d: " << d << std::endl;
+	  //extendedEuclidean()
+
+	  // Public Key
+	  file.open("e_n.txt");
+	  file << e << '\n' << n;
+	  file.close();
+	  // Private Key
+	  file.open("d_n.txt");
+	  file << d << '\n' << n;
+	  file.close();
+
+	  /*
       std::cout << "a couple of test cases for 3460:435/535 Algorithms!!!\n";
       BigUnsigned big1 = BigUnsigned(1);
       for (int i=0;i<400;i++) {
@@ -34,11 +71,39 @@ int main(){
       std::cout <<big3;
       std::cout << "my big3/big2 !!!\n";
       std::cout <<big3/big2;
-      
+	  */
 	} catch(char const* err) {
 		std::cout << "The library threw an exception:\n"
 			<< err << std::endl;
 	}
 
+
 	return 0;
+}
+
+/* This Function checks whether a bigUnsigned number that isn't one is prime or not,
+ * and will return a bool according to that value.
+ */
+bool fermatTest(BigUnsigned p)
+{
+	if (modexp(BigInteger(95), p-1, p) != 1) // single 1 being "1 mod p"
+		return false;
+	if (modexp(BigInteger(96), p-1, p) != 1) // single 1 being "1 mod p"
+		return false;
+	if (modexp(BigInteger(99), p-1, p) != 1) // single 1 being "1 mod p"
+		return false;
+	return true;
+}
+
+/* This function tries to generates a likely prime number */
+void makePrime(BigUnsigned &p)
+{
+	bool prime = false;
+	do {
+		p = 1;
+		for (int i(0); i < 400; i++)
+			p = p * 10 + rand();
+		prime = fermatTest(p);
+	}
+	while (!prime);
 }
