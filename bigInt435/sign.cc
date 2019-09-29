cc72a5a279f45281ee9898679cc5c9bd2f34d4bf
jrf115 <jrf115@zips.uakron.edu>
2019-09-29 16:16:29 -0400

    Initialize new files for Part 2
    Need to make sign.cc and include the SHA256 program from http://www.zedwood.com/article/cpp-sha256-function

---
+++ bigInt435/sign.cc
@@ -0,0 +1,29 @@
+/* John Fahringer
+ * Algorithms Fall 2019
+ * sign.cc  - Part2 of Project1
+ * This part will Generate and verify digigal signatures using a SHA-256 hash
+ **/
+
+ // Standard libraries
+ #include <string>
+ #include <iostream>
+ #include <stdlib.h>
+ #include <fstream>
+
+
+ // 'BigIntegerLibrary.hh' includes all of the library headers.
+ #include "BigIntegerLibrary.hh"
+ #include "sha256.h" /** Program by Olivier Gay */
+
+
+using std::string;
+using std::cout;
+using std::endl;
+
+int main() {
+
+
+
+
+    return 0;
+}
